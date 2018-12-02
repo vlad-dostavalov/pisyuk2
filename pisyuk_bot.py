fd45650c62a2a8244a8ce3ea732f8cd6566f3726
vlad-dostavalov <throbbingtorrent@gmail.com>
2018-12-02 13:50:34 +0300

    added debugs

--- pisyuk_bot.py
+++ pisyuk_bot.py
@@ -14,9 +14,12 @@ def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
-@bot.message_handler(regex="^(?!\(.*$).*")
+@bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
+    bot.reply_to(message, "gonnadoit")
     bot.send_message("@shorteez", message.text)
+    bot.send_message(message, "jobsdone")
+    return "ok", 200
 
 
 @server.route('/' + TOKEN, methods=['POST'])
